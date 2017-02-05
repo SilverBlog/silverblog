e29af641ede3e25f74985ecff4bf4f85f740bee3
qwe7002(workstation) <qwe7002@hotmail.com>
2017-02-05 21:23:09 +0800

    删除硬转换

--- init.py
+++ init.py
@@ -61,7 +61,7 @@ def LoadDocument(name):
         pageinfo = json.loads(Documents[0])
         Document = Documents[1]
     Document=markdown.html(Document, extensions=markdown.EXT_FENCED_CODE |
-                                                markdown.EXT_AUTOLINK | markdown.EXT_TABLES | markdown.EXT_STRIKETHROUGH,render_flags=markdown.HTML_HARD_WRAP)
+                                                markdown.EXT_AUTOLINK | markdown.EXT_TABLES | markdown.EXT_STRIKETHROUGH)
     Document = render_template("post.html", title=pageinfo["title"], pageinfo=pageinfo, menu_list=menu_list,
                                project_name=project_name, context=Document, author_image=author_image,
                                requestpage=request_page)
