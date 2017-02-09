ba1834f54e7397f66b53265a44d424f9fb205af2
qwe7002(workstation) <qwe7002@hotmail.com>
2017-02-09 18:46:58 +0800

    修改newpost算法

--- newpost.py
+++ newpost.py
@@ -26,17 +26,19 @@ def new_post(name,title,file,content=""):
         name=title
     name=getname(name)
     if len(content)!=0:
-        Document = open("./document/" + name + ".md","r", newline=None)
+        Document = open("./document/" + name + ".md","w", newline=None)
         Document.write(content)
-        Document=content
+        Document.close()
+        Document_raw=content
     else:
         if os.path.isfile(file):
             os.system("cp " + file + " ./document/" + name + ".md")
         else:
             os.system("vim ./document/" + name + ".md")
         Document = open("./document/" + name + ".md", newline=None)
-        Document = Document.read()
-    filetered = filter.filter_tags(markdown.html(Document))
+        Document_raw = Document.read()
+        Document.close()
+    filetered = filter.filter_tags(markdown.html(Document_raw))
     if len(filetered) > 140:
         excerpt = filetered[0:140]
     else:
@@ -69,4 +71,4 @@ if __name__ == '__main__':
         title = input("请输入文章标题: ")
         name = input("请输入文章URL(留空使用标题拼音):")
         file = input("请输入要复制的文件路径(留空或不存在将新建):")
-    new_post(name.title.file)
+    new_post(name,title,file)
