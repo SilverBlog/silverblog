eeffb93722c683fb296b6a3cd09e7c6e05e033da
qwe7002(workstation) <qwe7002@hotmail.com>
2017-02-19 20:47:43 +0800

    修改模版，修改安装脚本

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 sudo apt-get install nginx uwsgi uwsgi-plugin-python3 python3-pip memcached clang python python3-dev libffi libffi-dev
 sudo pip3 install -r python_package_list
 mkdir document
-cp ./config/menu.example.json menu.json
-cp ./config/page.example.json page.json
-python3 ./auto_config.py
+cp ./config/menu.example.json ./config/menu.json
+cp ./config/page.example.json ./config/page.json
+cp ./config/system.example.json ./config/system.json
+vim ./config/system.json
