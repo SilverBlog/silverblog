dacfe3be2a76742e52d395e8eb18d7d6c055282a
qwe7002(workstation) <qwe7002@hotmail.com>
2017-03-22 02:17:16 +0800

    修改controlserver和install脚本

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 sudo apt-get install nginx uwsgi uwsgi-plugin-python3 python3-pip memcached python3-dev libffi-dev
 sudo pip3 install -r ./python_package_list
 chmod +x manage.py
+chmod +x start.sh
 mkdir document
 cp ./config/menu.example.json ./config/menu.json
 cp ./config/page.example.json ./config/page.json
