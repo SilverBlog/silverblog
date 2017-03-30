2eadbd6f038892c6ad73e74d8a171ae197fbd062
qwe7002(workstation) <qwe7002@hotmail.com>
2017-03-30 21:16:58 +0800

    修改变量位置错误

--- init.py
+++ init.py
@@ -9,32 +9,32 @@ from flask import Flask, abort
 
 from common import file, page
 
-#Global Var
+# Global Var
 system_config = None
 menu_list = None
 rss = None
 restful_switch = False
 page_name_list = list()
 template_config = None
-cache_switch=False
-
+cache_switch = False
+page_list = None
+mc = None
 app = Flask(__name__)
 
-@@app.route('/managek/reload_config')
+
+@app.route('/managek/reload_config')
 def load_config():
-    global system_config,menu_list,rss,restful_switch,page_name_list,template_config,cache_switch
+    global system_config, menu_list, rss, restful_switch, page_name_list, template_config, cache_switch, mc, page_list
     system_config = json.loads(file.read_file("config/system.json"))
     system_config["API_Password"] = None
-    
+
     page_list = json.loads(file.read_file("./config/page.json"))
-    
+
     if os.path.exists("./config/menu.json"):
         menu_list = json.loads(file.read_file("./config/menu.json"))
-    
 
     if os.path.exists("document/rss.xml"):
         rss = file.read_file("document/rss.xml")
-    
 
     if "Restful_API" in system_config:
         if system_config["Restful_API"]:
@@ -42,7 +42,7 @@ def load_config():
 
     for item in page_list:
         page_name_list.append(item["name"])
-    
+
     cache_switch = system_config["Cache"]
     if cache_switch:
         mc = memcache.Client([system_config["Memcached_Connect"]], socket_timeout=500, debug=0)
@@ -52,7 +52,9 @@ def load_config():
         template_config = json.loads("./{0}/config.json".format(system_config["Theme"]))
     return "success"
 
+
 load_config()
+
 
 # Subscribe
 @app.route("/rss")
@@ -67,7 +69,7 @@ def rss():
 @app.route('/<file_name>/p/<page_index>')
 @app.route('/<file_name>/p/<page_index>/')
 def route(file_name="index", page_index="1"):
-    result=None
+    result = None
 
     if cache_switch:
         result = mc.get("{0}/p/{1}".format(file_name, str(page_index)))
@@ -77,9 +79,10 @@ def route(file_name="index", page_index="1"):
 
     if result is None:
         if file_name == "index":
-            result, row = page.build_index(int(page_index), system_config, page_list, menu_list, False,template_config)
+            result, row = page.build_index(int(page_index), system_config, page_list, menu_list, False, template_config)
         if os.path.exists("document/{0}.md".format(file_name)):
-            result = page.build_page(file_name, system_config, page_list, page_name_list, menu_list, False,template_config)
+            result = page.build_page(file_name, system_config, page_list, page_name_list, menu_list, False,
+                                     template_config)
 
     if result is not None:
         if cache_switch:
