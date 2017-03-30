71a1873da7bad400ad4544348deba4f89ea08137
qwe7002(workstation) <qwe7002@hotmail.com>
2017-03-30 16:50:42 +0800

    添加Template_config文件

--- common/page.py
+++ common/page.py
@@ -3,12 +3,13 @@ import os.path
 
 from jinja2 import Environment, PackageLoader
 
-from common import file,markdown
+from common import file, markdown
 
 env = Environment(loader=PackageLoader('init', 'templates'))
 
 
 def build_index(page, system_config, page_list, menu_list, static):
+    template_config = None
     page_info = {"title": "index"}
     Start_num = -system_config["Paging"] + (int(page) * system_config["Paging"])
     page_row_mod = divmod(len(page_list), system_config["Paging"])
@@ -21,17 +22,21 @@ def build_index(page, system_config, page_list, menu_list, static):
     if page <= 0 or page > page_row:
         return None
     index_list = page_list[Start_num:Start_num + system_config["Paging"]]
+    if os.path.exists("./{0}/config.json".format(system_config["Theme"])):
+        template_config = json.loads("./{0}/config.json".format(system_config["Theme"]))
     template = env.get_template("./{0}/index.html".format(system_config["Theme"]))
     result = template.render(menu_list=menu_list,
                              page_list=index_list,
                              page_info=page_info,
                              system_config=system_config,
+                             template_config=template_config,
                              page_row=page_row,
                              now_page=page, last_page=page - 1, next_page=page + 1, static=static)
     return result, page_row
 
 
 def build_page(name, system_config, page_list, page_name_list, menu_list, static):
+    template_config=None
     content = file.read_file("document/{0}.md".format(name))
     page_info = {"title": "undefined"}
     if name in page_name_list:
@@ -39,11 +44,14 @@ def build_page(name, system_config, page_list, page_name_list, menu_list, static
     if os.path.exists("document/{0}.json".format(name)):
         page_info = json.loads(file.read_file("document/{0}.json".format(name)))
     document = markdown.markdown(content)
+    if os.path.exists("./{0}/config.json".format(system_config["Theme"])):
+        template_config = json.loads("./{0}/config.json".format(system_config["Theme"]))
     template = env.get_template("./{0}/post.html".format(system_config["Theme"]))
     result = template.render(page_info=page_info, menu_list=menu_list, content=document,
-                             system_config=system_config, static=static)
+                             system_config=system_config, static=static, template_config=template_config)
 
     return result
+
 
 def build_restful_result(name, system_config, page_list, page_name_list, menu_list):
     content = None
