a94b75af1fa0026448e4caff762cca243c8dfbd0
qwe7002(workstation) <qwe7002@hotmail.com>
2017-04-05 17:16:02 +0800

    修改参数

--- manage/build_static_page.py
+++ manage/build_static_page.py
@@ -12,24 +12,28 @@ def build(github_mode):
     page_list = json.loads(file.read_file("./config/page.json"))
     menu_list = json.loads(file.read_file("./config/menu.json"))
     system_config = json.loads(file.read_file("./config/system.json"))
+    template_config = None
+    if os.path.exists("./templates/{0}/config.json".format(system_config["Theme"])):
+        template_config = json.loads(file.read_file("./templates/{0}/config.json".format(system_config["Theme"])))
+
     if os.path.isdir("./static_page"):
         shutil.rmtree("./static_page")
     os.mkdir("./static_page")
     page_name_list = list()
     for item in page_list:
         page_name_list.append(item["name"])
-    content, row = page.build_index(1, system_config, page_list, menu_list, html_static)
+    content, row = page.build_index(1, system_config, page_list, menu_list, html_static,template_config)
     file.write_file("./static_page/index.html", content)
     if row != 1:
         os.mkdir("./static_page/index/p")
         file.write_file("./static_page/index/p/1.html", "<meta http-equiv='refresh' content='0.1; url=/'>")
         for page_id in range(2, row + 1):
-            content, row = page.build_index(page_id, system_config, page_list, menu_list, html_static)
+            content, row = page.build_index(page_id, system_config, page_list, menu_list, html_static,template_config)
             file.write_file("./static_page/index/p/{0}.html".format(str(page_id)), content)
     for filename in os.listdir("./document/"):
         if filename.endswith(".md"):
             content = page.build_page(filename.replace(".md", ""), system_config, page_list, page_name_list, menu_list,
-                                      True)
+                                      html_static, template_config)
             if content is not None:
                 file.write_file("./static_page/{0}.html".format(filename.replace(".md", "")), content)
     shutil.copyfile("./document/rss.xml", "./static_page/rss.xml")
