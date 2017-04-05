3728a8c9b7bddbfb17daacaa168fb1c8ad24f5bf
qwe7002(workstation) <qwe7002@hotmail.com>
2017-04-05 17:10:41 +0800

    修改manage实现

--- manage.py
+++ manage.py
@@ -7,9 +7,10 @@ from manage import build_rss, build_static_page, new_post, update_post
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("command", help="The name of the function to execute.(e.g: new,update,build-rss,build-gh-page,build-static-page)")
+    parser.add_argument("command", help="The name of the function to execute.(e.g: new,update,build-rss,build-gh-page)")
     parser.add_argument("--config", help="The configuration file location you want to load.")
     parser.add_argument("--editor", help="Your favorite editor(e.g: vim,nano,gedit)")
+    parser.add_argument("--static_page",help="Create page that is available to static server",action="store_true")
     args = parser.parse_args()
     if args.command == "new":
         config=None
@@ -24,7 +25,4 @@ if __name__ == '__main__':
         build_rss.build_rss()
     if args.command == "build-gh-page":
         build_rss.build_rss()
-        build_static_page.build(False)
-    if args.command == "build-static-page":
-        build_rss.build_rss()
-        build_static_page.build(True)
+        build_static_page.build(args.static_page)
