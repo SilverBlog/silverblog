485d2f32ac06f966d8bb4b3b7ae7a6efb00010c4
qwe7002(workstation) <qwe7002@hotmail.com>
2017-04-06 01:06:28 +0800

    删除读写log

--- common/file.py
+++ common/file.py
@@ -1,13 +1,10 @@
-from common import console
 def read_file(filename):
-    console.log("info","Read the file: {0}".format(filename))
     f = open(filename, newline=None)
     content = f.read()
     return content
 
 
 def write_file(filename, content):
-    console.log("info","Write the file: {0}".format(filename))
     f = open(filename, "w", newline=None)
     f.write(content)
     f.close()
