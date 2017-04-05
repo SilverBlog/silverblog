9401a42d611f866fbe155c6df60797933be0e866
qwe7002(workstation) <qwe7002@hotmail.com>
2017-04-05 16:59:56 +0800

    修改static分页html位置

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from manage import build_rss, build_static_page, new_post, update_post
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("command", help="The name of the function to execute.(e.g: new,update,build-rss,build-gh-page)")
+    parser.add_argument("command", help="The name of the function to execute.(e.g: new,update,build-rss,build-gh-page,build-static-page)")
     parser.add_argument("--config", help="The configuration file location you want to load.")
     parser.add_argument("--editor", help="Your favorite editor(e.g: vim,nano,gedit)")
     args = parser.parse_args()
@@ -24,4 +24,7 @@ if __name__ == '__main__':
         build_rss.build_rss()
     if args.command == "build-gh-page":
         build_rss.build_rss()
-        build_static_page.build()
+        build_static_page.build(False)
+    if args.command == "build-static-page":
+        build_rss.build_rss()
+        build_static_page.build(True)
--- manage/build_static_page.py
+++ manage/build_static_page.py
@@ -5,7 +5,10 @@ import shutil
 from common import file,page
 
 
-def build():
+def build(github_mode):
+    html_static=False
+    if github_mode is not None:
+        html_static=github_mode
     page_list = json.loads(file.read_file("./config/page.json"))
     menu_list = json.loads(file.read_file("./config/menu.json"))
     system_config = json.loads(file.read_file("./config/system.json"))
@@ -15,13 +18,14 @@ def build():
     page_name_list = list()
     for item in page_list:
         page_name_list.append(item["name"])
-    content, row = page.build_index(1, system_config, page_list, menu_list, True)
+    content, row = page.build_index(1, system_config, page_list, menu_list, html_static)
     file.write_file("./static_page/index.html", content)
     if row != 1:
-        file.write_file("./static_page/index_1.html", "<meta http-equiv='refresh' content='0.1; url=/'>")
+        os.mkdir("./static_page/index/p")
+        file.write_file("./static_page/index/p/1.html", "<meta http-equiv='refresh' content='0.1; url=/'>")
         for page_id in range(2, row + 1):
-            content, row = page.build_index(page_id, system_config, page_list, menu_list, True)
-            file.write_file("./static_page/index_{0}.html".format(str(page_id)), content)
+            content, row = page.build_index(page_id, system_config, page_list, menu_list, html_static)
+            file.write_file("./static_page/index/p/{0}.html".format(str(page_id)), content)
     for filename in os.listdir("./document/"):
         if filename.endswith(".md"):
             content = page.build_page(filename.replace(".md", ""), system_config, page_list, page_name_list, menu_list,
