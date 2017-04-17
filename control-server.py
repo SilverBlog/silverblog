f7a41fab497217353b2508aac9f83d8bc51cb317
qwe7002(workstation) <qwe7002@hotmail.com>
2017-04-17 20:50:46 +0800

    修改测试密码部分

--- control-server.py
+++ control-server.py
@@ -8,20 +8,25 @@ from manage import new_post
 
 app = Flask(__name__)
 
+system_config = json.loads(file.read_file("config/system.json"))
+
+
+def check_password(title, encode):
+    hash_md5 = hashlib.md5(str(title + system_config["API_Password"]).encode('utf-8')).hexdigest()
+    if encode == hash_md5:
+        return True
+    return False
 
 @app.route('/control/new', methods=['POST'])
 def new():
     title = request.json["title"]
     content = request.json["content"]
     encode = request.json["encode"]
-    system_config = json.loads(file.read_file("config/system.json"))
-    hash_md5 = hashlib.md5(str(title + system_config["API_Password"]).encode('utf-8')).hexdigest()
-    if encode == hash_md5 and len(content) != 0:
+    state = "error"
+    if check_password(title, encode):
         name = new_post.get_name(str(title))
         file.write_file("./document/{0}.md".format(name), str(content))
-        config_raw={"title":str(title),"name":name,"file":"./document/{0}.md".format(name)}
-        config=json.dumps(config_raw)
+        config = json.dumps({"title": str(title), "name": name, "file": "./document/{0}.md".format(name)})
         new_post.new_post_init(config, None)
-        return '{"status":"ok"}'
-    else:
-        return '{"status":"no"}'
+        state = "ok"
+    return json.dumps({"state": state})
