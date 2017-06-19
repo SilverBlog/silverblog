fa91f5708f2dbfa19ce7b91b9f8a25730d796382
qwe7002(workstation) <qwe7002@hotmail.com>
2017-06-19 11:20:52 +0800

    实现postlist方法

--- control_server.py
+++ control_server.py
@@ -1,6 +1,6 @@
+# coding=utf-8
 import hashlib
 import json
-
 from flask import Flask, request
 
 from common import file
@@ -10,6 +10,7 @@ app = Flask(__name__)
 
 system_config = json.loads(file.read_file("./config/system.json"))
 
+page_list = json.loads(file.read_file("./config/page.json"))
 
 def check_password(title, encode):
     hash_md5 = hashlib.md5(str(title + system_config["API_Password"]).encode('utf-8')).hexdigest()
@@ -17,6 +18,27 @@ def check_password(title, encode):
         return True
     return False
 
+
+@app.route('/control/get_post_list', methods=['POST'])
+def post_list():
+    encode = str(request.json["encode"])
+    if check_password("get_post_list", encode):
+        for item in page_list:
+            page_name_list.append(item["title"])
+    return json.dumps(page_name_list)
+
+
+@app.route('/control/edit', methods=['POST'])
+def edit_post():
+    post_id = int(request.json["post_id"])
+    title = int(request.json["title"])
+    content = str(request.json["content"])
+    encode = str(request.json["encode"])
+    if check_password(post_id, encode):
+
+
+# todo edit设计
+# return json.dumps()
 @app.route('/control/new', methods=['POST'])
 def new():
     title = str(request.json["title"])
