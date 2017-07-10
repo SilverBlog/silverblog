e0eeadc613883db553c01f8d4152e389a7fe3932
qwe7002(workstation) <qwe7002@hotmail.com>
2017-07-10 17:54:36 +0800

    删除config文件夹

--- install/debian_install.sh
+++ install/debian_install.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 sudo apt-get install nginx uwsgi uwsgi-plugin-python3 python3-pip python3-wheel
+sudo pip3 install cffi
+sudo pip3 install flask misaka pypinyin pyrss2gen
 ./install.sh
--- install/install.sh
+++ install/install.sh
@@ -18,16 +18,15 @@ server {
 }
 EOF
 
-sudo pip3 install cffi
-sudo pip3 install flask misaka pypinyin pyrss2gen
 git submodule init
 git submodule update
 mkdir document
 mkdir templates/static
+mkdir config
 
-cp -i config/menu.example.json config/menu.json
-cp -i config/page.example.json config/page.json
-cp -i config/system.example.json config/system.json
+cp -i example/menu.example.json config/menu.json
+cp -i example/page.example.json config/page.json
+cp -i example/system.example.json config/system.json
 
 cp -i example/start.example.json start.json
 cp -i example/start.example.sh start.sh
