51a0cc5d55d93fb4010af57c3f62d460bf022965
tianweb <tianiwap@gmail.com>
2017-08-06 11:22:40 +0800

    modified:   install/archlinux_install.sh

--- install/archlinux_install.sh
+++ install/archlinux_install.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-#NextMoe-icecat by 20170806 03:16
-sudo pacman -S nginx uwsgi uwsgi-plugin-python python-pip python-wheel sudo gcc vim
+#NextMoe-icecat modified 20170806 03:16(Aisa/Shanghai)
+sudo pacman -S nginx uwsgi uwsgi-plugin-python python-pip python-wheel sudo git gcc vim
 
 sudo pip install cffi
-sudo pip install flask misaka 
+sudo pip install flask 
+sudo pip install misaka 
 sudo pip install pypinyin 
-sudo pip install pyrss2gen gitpython
+sudo pip install pyrss2gen 
+sudo pip install gitpython
 
 if [ ! -f "install.sh" ]; then
     git clone https://github.com/SilverBlogTeam/SilverBlog.git
