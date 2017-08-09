50a4a6ac12fdfe644bd530055f5888dda0233624
qwe7002 <qwe7002@hotmail.com>
2017-08-09 17:22:27 +0800

    添加安装log输出

--- install/debian_install.sh
+++ install/debian_install.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+
+echo Updating software source
 sudo apt-get update
+
+echo Installing Dependency
 sudo apt-get install -y nginx uwsgi uwsgi-plugin-python3 python3-pip python3-wheel git vim
 sudo pip3 install cffi
 sudo pip3 install flask misaka pypinyin pyrss2gen gitpython
+
 if [ ! -f "install.sh" ]; then
     git clone https://github.com/SilverBlogTeam/SilverBlog.git
     cd SilverBlog/install
