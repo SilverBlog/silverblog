2805339797685ccccf569b11ab2287c075547b4b
qwe7002 <qwe7002@hotmail.com>
2017-08-10 22:18:10 +0800

    修改安装脚本

--- install/archlinux_install.sh
+++ install/archlinux_install.sh
@@ -2,19 +2,15 @@
 #NextMoe-icecat modified 20170806 03:16(Asia/Shanghai)
 #Reall Computer modified 20170810 16:16(Asia/Shanghai)
 echo "Installing Dependency..."
-sudo pacman -S nginx uwsgi uwsgi-plugin-python python-pip python-wheel sudo git gcc vim
+pacman -S nginx uwsgi uwsgi-plugin-python python-pip python-wheel sudo git gcc vim
 
-sudo pip install cffi
-sudo pip install flask 
-sudo pip install misaka 
-sudo pip install pypinyin 
-sudo pip install pyrss2gen 
-sudo pip install gitpython
+pip install cffi
+pip install flask misaka pypinyin pyrss2gen gitpython
 
 if [ ! -f "install.sh" ]; then
     git clone https://github.com/SilverBlogTeam/SilverBlog.git
     cd SilverBlog/install
 fi
-sudo chmod +x install.sh
+chmod +x install.sh
 ./install.sh
  
