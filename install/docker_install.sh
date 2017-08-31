1cf5a946955c1810ef40fd798241f68fe3898257
qwe7002 <qwe7002@hotmail.com>
2017-08-31 13:57:02 +0800

    修改安装脚本错误

--- install/docker_install.sh
+++ install/docker_install.sh
@@ -53,11 +53,11 @@ sed -i '''s/127.0.0.1/0.0.0.0/g' uwsgi.json
 
 cat << EOF >./start.sh
 #!/usr/bin/env bash
-docker run -t -i -v $x/home/SilverBlog -p 5000:5000 silverblog uwsgi --json /home/SilverBlog/uwsgi.json
+docker run -t -i -v $x:/home/SilverBlog -p 5000:5000 silverblog uwsgi --json /home/SilverBlog/uwsgi.json --chdir /home/SilverBlog
 EOF
 cat << EOF >./control-start.sh
 #!/usr/bin/env bash
-docker run -t -i -v $x:/home/SilverBlog -p 5001:5001 silverblog uwsgi --json /home/SilverBlog/uwsgi.json:control
+docker run -t -i -v $x:/home/SilverBlog -p 5001:5001 silverblog uwsgi --json /home/SilverBlog/uwsgi.json:control --chdir /home/SilverBlog
 EOF
 
 
