8789db679c3d6af8858d38ab76979bd8c1c7a0e7
qwe7002 <qwe7002@hotmail.com>
2017-09-09 00:43:19 +0800

    修改docker输出

--- install/docker_install.sh
+++ install/docker_install.sh
@@ -7,11 +7,11 @@ fi
 
 cat << EOF >./start.sh
 #!/usr/bin/env bash
-docker run -v $x:/home/SilverBlog -p 5000:5000 silverblog uwsgi --json /home/SilverBlog/uwsgi.json --chdir /home/SilverBlog
+docker run -v $x:/home/SilverBlog -p 5000:5000 qwe7002/silverblog uwsgi --json /home/SilverBlog/uwsgi.json --chdir /home/SilverBlog
 EOF
 cat << EOF >./control-start.sh
 #!/usr/bin/env bash
-docker run -v $x:/home/SilverBlog -p 5001:5001 silverblog uwsgi --json /home/SilverBlog/uwsgi.json:control --chdir /home/SilverBlog
+docker run -v $x:/home/SilverBlog -p 5001:5001 qwe7002/silverblog uwsgi --json /home/SilverBlog/uwsgi.json:control --chdir /home/SilverBlog
 EOF
 
 ./install.sh
