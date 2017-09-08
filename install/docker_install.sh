756a2d70646681144ef4229f2c2761a07b5dfa07
qwe7002 <qwe7002@hotmail.com>
2017-09-09 00:59:58 +0800

    修复错误

--- install/docker_install.sh
+++ install/docker_install.sh
@@ -5,21 +5,15 @@ if [ ! -f "install.sh" ]; then
     cd SilverBlog/install
 fi
 
-cat << EOF >../control-start.sh
-#!/usr/bin/env bash
-docker run -v $x:/home/SilverBlog -p 5000:5000 qwe7002/silverblog uwsgi --json /home/SilverBlog/uwsgi.json --chdir /home/SilverBlog
-EOF
-cat << EOF >../control-start.sh
-#!/usr/bin/env bash
-docker run -v $x:/home/SilverBlog -p 5001:5001 qwe7002/silverblog uwsgi --json /home/SilverBlog/uwsgi.json:control --chdir /home/SilverBlog
-EOF
-
 ./install.sh
 
-sed -i '''s/127.0.0.1/0.0.0.0/g' uwsgi.json
+cat << EOF >../control-start.sh
+#!/usr/bin/env bash
+docker run -v $(pwd):/home/SilverBlog -p 5000:5000 qwe7002/silverblog uwsgi --json /home/SilverBlog/uwsgi.json --chdir /home/SilverBlog
+EOF
+cat << EOF >../start.sh
+#!/usr/bin/env bash
+docker run -v $(pwd):/home/SilverBlog -p 5001:5001 qwe7002/silverblog uwsgi --json /home/SilverBlog/uwsgi.json:control --chdir /home/SilverBlog
+EOF
 
-
-
-
-
-echo "The installation is complete! Please edit $x/config/system.json file."
+sed -i '''s/127.0.0.1/0.0.0.0/g' ./uwsgi.json
