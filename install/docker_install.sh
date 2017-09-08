b90c2acf58246c98364582a433698bd991aaa89a
qwe7002 <qwe7002@hotmail.com>
2017-09-09 01:12:30 +0800

    修复参数

--- install/docker_install.sh
+++ install/docker_install.sh
@@ -9,11 +9,11 @@ fi
 
 cd ..
 
-cat << EOF >control-start.sh
+cat << EOF >start.sh
 #!/usr/bin/env bash
 docker run -v $(pwd):/home/SilverBlog -p 5000:5000 qwe7002/silverblog uwsgi --json /home/SilverBlog/uwsgi.json --chdir /home/SilverBlog
 EOF
-cat << EOF >start.sh
+cat << EOF >control-start.sh
 #!/usr/bin/env bash
 docker run -v $(pwd):/home/SilverBlog -p 5001:5001 qwe7002/silverblog uwsgi --json /home/SilverBlog/uwsgi.json:control --chdir /home/SilverBlog
 EOF
