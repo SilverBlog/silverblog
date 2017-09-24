dd6752748ebea46ae160613441dbd8c57a4032a6
qwe7002 <qwe7002@hotmail.com>
2017-09-25 01:03:48 +0800

    添加theme

--- manage/theme.py
+++ manage/theme.py
@@ -1,10 +1,13 @@
+import urllib.request
+
 def get_theme_list():
-    import urllib.request
     console.log("info", "Get Theme List")
     r = "[]"
     try:
-        r = urllib.request.urlopen("https://api.github.com/orgs/silverblogtheme/repos"").read().decode('utf-8')
+        r = urllib.request.urlopen("https://api.github.com/orgs/silverblogtheme/repos").read().decode('utf-8')
     except urllib2.HTTPError:
         console.log("Error", "Get Theme List error")
         exit(1)
     req = json.loads(r)
+    for item in req:
+        print("Name:{0} Description:{1} Star{2}".format(item["name"], item["description"], item["stargazers_count"]))
