c8b9615c01e3df51f1945ac79a612f18909a9bef
qwe7002 <qwe7002@hotmail.com>
2017-09-25 01:17:19 +0800

    添加分割线

--- manage/theme.py
+++ manage/theme.py
@@ -14,4 +14,7 @@ def get_theme_list():
         exit(1)
     req = json.loads(r)
     for item in req:
-        print("Name:{0} Description:{1} Star{2}".format(item["name"], item["description"], item["stargazers_count"]))
+        print("Name:{0}\n Description:{1}\n Star:{2}\n------------------------------\n".format(item["name"],
+                                                                                               item["description"],
+                                                                                               item[
+                                                                                                   "stargazers_count"]))
