e0e7caf562995f3917beb7b183a5eaf80a2a4020
qwe7002 <qwe7002@hotmail.com>
2017-09-27 02:10:52 +0800

    实现自动化启用

--- manage/theme.py
+++ manage/theme.py
@@ -2,7 +2,7 @@ import json
 import os
 import urllib.request
 
-from common import console
+from common import console, file
 
 
 def get_orgs_list():
@@ -30,10 +30,12 @@ def install_theme(theme_name):
     req = get_orgs_list()
     has_theme = False
     full_name = None
+    name = None
     for item in req:
-        if item["name"] == theme_name:
+        if item["name"].lower() == theme_name.lower():
             has_theme = True
             full_name = item["full_name"]
+            name = item["name"]
     if not has_theme:
         console.log("Error", "Can not find this theme.")
         return
@@ -45,6 +47,12 @@ def install_theme(theme_name):
     except urllib2.HTTPError:
         console.log("Error", "Get the theme installation script error.")
         exit(1)
-
     os.system("cd templates \n" + r)
+    enable_theme = input('Do you want to enable this theme now? [y/N]')
+    if enable_theme.lower() == 'yes' or enable_theme.lower() == 'y' or enable_theme.lower() == '':
+        system_info = json.loads(file.read_file("./config/system.json"))
+        system_info["Theme"] = name
+        file.write_file("./config/system.json", json.dumps(system_info))
+        console.log("Success", "The theme has been enabled!")
     console.log("Success", "The theme is installed successfully!")
+
