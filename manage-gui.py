244f9f84f74f21f903ec9b86405bdcc860518dc3
qwe7002 <qwe7002@hotmail.com>
2017-10-17 23:59:57 +0800

    避免退出，添加exit选项

--- manage-gui.py
+++ manage-gui.py
@@ -1,70 +1,82 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import time
+
 from common import whiptail
 from manage import build_rss, build_static_page, new_post, update_post, theme
 
 dialog = whiptail.Whiptail()
 dialog.height = 15
 dialog.title = "SilverBlog command line management tool"
-menu_list = ["New post", "Update post", "Upgrade", "Theme package manager", "Build static page"]
-result = dialog.menu("Please select an action", menu_list)
-if result == "New post":
-    dialog.title = "New post"
-    title = dialog.prompt("Please enter the title of the article:")
-    if len(title) == 0:
-        dialog.alert("The title can not be blank.")
-        exit(1)
-    name = dialog.prompt("Please enter the URL (Leave a blank use pinyin):")
-    if len(name) == 0:
-        name = new_post.get_name(title)
-    config = {"title": title, "name": name}
-    new_post.new_post_init(config, dialog.confirm("Is this an independent page?", "no"))
-    build_rss.build_rss()
-    exit(0)
-if result == "Upgrade":
-    from manage import upgrade
 
-    if upgrade.upgrade_check():
-        if dialog.confirm("Find new version, do you want to upgrade?", "no"):
-            upgrade.upgrade_pull()
-            exit(0)
-    dialog.alert("No upgrade found")
-    exit(0)
-if result == "Update post":
-    update_post.update()
-    build_rss.build_rss()
-    exit(0)
-if result == "Theme package manager":
-    dialog.title = "Theme package manager"
-    menu_list = ["Install the theme", "Use the existing theme", "Upgrade existing Theme", "Uninstall existing Theme"]
+def loop_ui():
+    menu_list = ["New post", "Update post", "Upgrade", "Theme package manager", "Build static page", "Exit"]
     result = dialog.menu("Please select an action", menu_list)
-    theme_name = ""
-    orgs_list = None
-    if result == "Install the theme":
-        install_menu = ["View list", "Enter the theme package name"]
-        if result == "View list":
-            orgs_list = theme.get_orgs_list()
-            item_list = list()
-            for item in orgs_list:
-                item_list.append(item["name"])
-            theme_name = dialog.menu("Please select the theme you want to install:", item_list)
-        if result == "Enter the theme package name":
-            theme_name = dialog.prompt("Please enter the theme package name:")
-        if len(theme_name) != 0:
-            theme.install_theme(theme_name, orgs_list)
-            exit(0)
-    directories = theme.get_local_theme_list()
-    theme_name = dialog.menu("Please select the theme to be operated:", directories)
-    if result == "Use the existing theme":
-        theme.set_theme(theme_name)
-    if result == "Upgrade existing Theme":
-        theme.upgrade_theme(theme_name)
-    if result == "Uninstall existing Theme":
-        theme.remove_theme(theme_name)
-    exit(0)
-if result == "Build static page":
-    dialog.title = "Build static page"
-    build_static_page.publish(dialog.confirm("Push to git?", "no"),
-                              dialog.confirm("Generate a hyperlink with a file extension?", "no"))
-    exit(0)
+    if result == "Exit":
+        exit(0)
+    if result == "New post":
+        dialog.title = "New post"
+        title = dialog.prompt("Please enter the title of the article:")
+        if len(title) == 0:
+            dialog.alert("The title can not be blank.")
+            return
+        name = dialog.prompt("Please enter the URL (Leave a blank use pinyin):")
+        if len(name) == 0:
+            name = new_post.get_name(title)
+        config = {"title": title, "name": name}
+        new_post.new_post_init(config, dialog.confirm("Is this an independent page?", "no"))
+        build_rss.build_rss()
+        return
+    if result == "Upgrade":
+        from manage import upgrade
+
+        if upgrade.upgrade_check():
+            if dialog.confirm("Find new version, do you want to upgrade?", "no"):
+                upgrade.upgrade_pull()
+                return
+        dialog.alert("No upgrade found")
+        return
+    if result == "Update post":
+        update_post.update()
+        build_rss.build_rss()
+        return
+    if result == "Theme package manager":
+        dialog.title = "Theme package manager"
+        menu_list = ["Install the theme", "Use the existing theme", "Upgrade existing Theme",
+                     "Uninstall existing Theme"]
+        result = dialog.menu("Please select an action", menu_list)
+        theme_name = ""
+        orgs_list = None
+        if result == "Install the theme":
+            install_menu = ["View list", "Enter the theme package name"]
+            result = dialog.menu("Please select an action", install_menu)
+            if result == "View list":
+                orgs_list = theme.get_orgs_list()
+                item_list = list()
+                for item in orgs_list:
+                    item_list.append(item["name"])
+                theme_name = dialog.menu("Please select the theme you want to install:", item_list)
+            if result == "Enter the theme package name":
+                theme_name = dialog.prompt("Please enter the theme package name:")
+            if len(theme_name) != 0:
+                theme.install_theme(theme_name, orgs_list)
+            return
+        directories = theme.get_local_theme_list()
+        theme_name = dialog.menu("Please select the theme to be operated:", directories)
+        if result == "Use the existing theme":
+            theme.set_theme(theme_name)
+        if result == "Upgrade existing Theme":
+            theme.upgrade_theme(theme_name)
+        if result == "Uninstall existing Theme":
+            theme.remove_theme(theme_name)
+        return
+    if result == "Build static page":
+        dialog.title = "Build static page"
+        build_static_page.publish(dialog.confirm("Push to git?", "no"),
+                                  dialog.confirm("Generate a hyperlink with a file extension?", "no"))
+        return
+
+while True:
+    loop_ui()
+    time.sleep(0.5)
