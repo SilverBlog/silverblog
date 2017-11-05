b45e8d7321bb80d3538abbad671c0314bdf1b010
qwe7002 <qwe7002@hotmail.com>
2017-11-06 01:38:00 +0800

    修改安装流程

--- install/debian_install.sh
+++ install/debian_install.sh
@@ -15,11 +15,6 @@ echo "Installing Dependency..."
 $use_superuser apt-get install -y nginx uwsgi uwsgi-plugin-python3 python3-pip python3-wheel git
 $use_superuser pip3 install -r python_dependency.txt
 
-read -p "Is qrcode support component installed? (Y/N): " yn
-
-if [ "$yn" == "Y" ] || [ "$yn" == "y" ]; then
-    $use_superuser pip3 install qrcode-terminal
-fi
 
 if [ ! -f "install.sh" ]; then
     git clone https://github.com/SilverBlogTeam/SilverBlog.git --depth=1
@@ -29,6 +24,12 @@ fi
 if [ ! -f "../start.json" ]; then
     cp -i ../example/start.json ../start.json
 
+fi
+
+read -p "Is qrcode support component installed? (Y/N): " yn
+
+if [ "$yn" == "Y" ] || [ "$yn" == "y" ]; then
+    $use_superuser pip3 install qrcode-terminal
 fi
 
 ./install.sh
