98f7dd28c59c6859d660f6e46e8b018b369e000b
qwe7002 <qwe7002@hotmail.com>
2017-11-16 16:45:25 +0800

    修改菜单设计

--- manage.py
+++ manage.py
@@ -12,23 +12,24 @@ if __name__ == '__main__':
         exit(0)
     parser = argparse.ArgumentParser("SilverBlog CLI tool.")
     parser.add_argument("command", help="The name of the function to execute.")
+
     #new
     group_new = parser.add_argument_group('new', "Create a new article.")
-    group_new.add_argument("--config", help="The configuration file location you want to load.")
-    group_new.add_argument("--independent", help="Generate an article that does not appear in the article list",
+    group_new.add_argument("-c", "--config", help="The configuration file location you want to load.", type=str)
+    group_new.add_argument("-i", "--independent", help="Generate an article that does not appear in the article list",
                            action="store_true")
 
     parser.add_argument_group('update', "Update article metadata.")
+
     parser.add_argument_group('theme-install', "Theme pack installation management.")
 
-    #theme
     parser.add_argument_group('upgrade', "Upgrade program")
 
     #build-gh-page
     group_build_gh_page = parser.add_argument_group("build-gh-page", "Generate static pages.")
-    group_build_gh_page.add_argument("--static_page", help="Create page that is available to static server",
+    group_build_gh_page.add_argument("-s", "--static_page", help="Create page that is available to static server",
                                      action="store_true")
-    group_build_gh_page.add_argument("--push_git", help="Automatically submitted to Git", action="store_true")
+    group_build_gh_page.add_argument("-p", "--push_git", help="Automatically submitted to Git", action="store_true")
 
     args = parser.parse_args()
     try:
