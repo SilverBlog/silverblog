8640a267558c4f6d92fdf6f963e2fc66e7f42e64
qwe7002 <qwe7002@hotmail.com>
2017-12-03 11:46:34 +0800

    修改函数位置

--- manage/build_static_page.py
+++ manage/build_static_page.py
@@ -6,6 +6,19 @@ import time
 
 from common import console
 
+@asyncio.coroutine
+def build_post_page(filename, page_name_list):
+    file_name = filename.replace(".md", "")
+    console.log("Build", "Processing file: ./static_page/post/{0}.html".format(file_name))
+    page_info = None
+    if file_name in page_name_list:
+        this_page_index = page_name_list.index(file_name)
+        page_info = page_list[this_page_index]
+    content = page.build_page(file_name, system_config, page_info, menu_list,
+                              html_static, template_config)
+    if content is not None:
+        file.write_file("./static_page/post/{0}.html".format(filename.replace(".md", "")), content)
+
 def build(github_mode):
     from common import file, page, post_map
     html_static = False
@@ -40,12 +53,12 @@ def build(github_mode):
             content, row = page.build_index(page_id, system_config, page_list, menu_list, html_static, template_config)
             file.write_file("./static_page/index/p/{0}.html".format(str(page_id)), content)
     os.mkdir("./static_page/post/")
-    loop = asyncio.get_event_loop()
+
     for filename in os.listdir("./document/"):
         if filename.endswith(".md"):
             tasks = list()
             tasks.append(build_post_page(filename, page_name_list))
-
+    loop = asyncio.get_event_loop()
     loop.run_until_complete(asyncio.gather(*tasks))
     loop.close()
     shutil.copyfile("./document/rss.xml", "./static_page/rss.xml")
@@ -54,19 +67,6 @@ def build(github_mode):
     if os.path.exists("./templates/static/user_file"):
         shutil.copytree("./templates/static/user_file", "./static_page/static/user_file")
     console.log("Success", "Create Github Page Success!")
-
-@asyncio.coroutine
-def build_post_page(filename, page_name_list):
-    file_name = filename.replace(".md", "")
-    console.log("Build", "Processing file: ./static_page/post/{0}.html".format(file_name))
-    page_info = None
-    if file_name in page_name_list:
-        this_page_index = page_name_list.index(file_name)
-        page_info = page_list[this_page_index]
-    content = page.build_page(file_name, system_config, page_info, menu_list,
-                              html_static, template_config)
-    if content is not None:
-        file.write_file("./static_page/post/{0}.html".format(filename.replace(".md", "")), content)
 
 def publish(push, static):
     if not os.path.exists("./static_page/.git"):
