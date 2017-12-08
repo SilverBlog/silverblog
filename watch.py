a7ce6a772aaf295c526944edb04931ccfeef04b7
qwe7002 <qwe7002@hotmail.com>
2017-12-08 23:33:19 +0800

    删除control服务器参数控制

--- watch.py
+++ watch.py
@@ -38,8 +38,6 @@ if __name__ == "__main__":
 
     if args.control:
         control_cmd = ["uwsgi", "--json", "uwsgi.json:control"]
-        if args.docker:
-            control_cmd.extend(["--worker-reload-mercy", "1", "--reload-mercy", "4"])
         control_p = subprocess.Popen(control_cmd, stderr=subprocess.PIPE)
 
     cmd = ["uwsgi", "--json", "uwsgi.json"]
