8c060f7da97d0794411e4df0116994a975880682
qwe7002 <qwe7002@hotmail.com>
2017-12-10 20:08:55 +0800

    修复拼写错误

--- manage/delete_post.py
+++ manage/delete_post.py
@@ -7,4 +7,4 @@ def delete(page_list, post_index):
     file_name = page_list[post_index]["name"]
     del page_list[post_index]
     file.write_file("./config/page.json", json.dumps(page_list, indent=4, sort_keys=False, ensure_ascii=False))
-    os.remove("./document/{0}.md".format(ile_name))
+    os.remove("./document/{0}.md".format(file_name))
