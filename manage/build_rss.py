64c8cb495a060b56c11c29567066df8518c5ed39
qwe7002 <qwe7002@hotmail.com>
2017-12-10 15:38:34 +0800

    设置range

--- manage/build_rss.py
+++ manage/build_rss.py
@@ -46,7 +46,7 @@ def make_rss_item(page_list, item, project_url):
                                                     pubDate=datetime.datetime.fromtimestamp(item["time"]))
 
 def make_rss(project_name, project_url, project_description, page_list, system_config):
-    rss_item_list = range(0, len(page_list))
+    rss_item_list = range(1, len(page_list))
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     tasks = [make_rss_item(page_list, item, system_config["Project_URL"]) for item in page_list]
