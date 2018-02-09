97801fb1d54d80095b60237c4acaf34c47359ca8
qwe7002 <qwe7002@hotmail.com>
2018-02-10 00:09:00 +0800

    修改编码

--- install/container_install.sh
+++ install/container_install.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
-
 git clone https://github.com/SilverBlogTeam/SilverBlog.git --depth=1 ./
-
 if [ ! -f "./docker-compose.yml" ]; then
     cp -i ./example/docker-compose.example.yml ./docker-compose.yml
 fi
-
 cat << EOF >manage.sh
 #!/usr/bin/env bash
 docker run -it -v ./:/home/silverblog --name="silverblog_manage"  silverblog/silverblog python3 manage.py
 EOF
-
 ./install/install.sh
 sed -i '''s/127.0.0.1/0.0.0.0/g' uwsgi.json
