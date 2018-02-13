62755541c1101e655514bf02aa5d3a5355f26145
qwe7002 <qwe7002@hotmail.com>
2018-02-14 00:39:37 +0800

    安装脚本修改

--- install/archlinux_install.sh
+++ install/archlinux_install.sh
@@ -7,22 +7,6 @@ if [ $UID -ne 0 ]; then
     use_superuser="sudo"
 fi
 
-V1=3
-V2=4
-echo need python version is : $V1.$V2
-U_V1=`python -V 2>&1|awk '{print $2}'|awk -F '.' '{print $1}'`
-U_V2=`python -V 2>&1|awk '{print $2}'|awk -F '.' '{print $2}'`
-echo your python version is : $U_V1.$U_V2
-
-if [ $U_V1 -lt $V1 ];then
-    echo 'The current Python version is below 3.4 and can not install SilverBlog'
-    exit 1
-fi
-if [ $U_V2 -lt $V2 ];then
-    echo 'The current Python version is below 3.4 and can not install SilverBlog'
-    exit 1
-fi
-
 echo "Installing Dependency..."
 ${use_superuser} pacman -Sy nginx uwsgi python libnewt uwsgi-plugin-python python-pip python-wheel git
 ${use_superuser} pip install -r python_dependency.txt
