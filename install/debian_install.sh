b960661db449d870dc4eb93b84186f8163e7e29f
qwe7002 <qwe7002@hotmail.com>
2018-02-14 19:53:28 +0800

    安装脚本修改

--- install/debian_install.sh
+++ install/debian_install.sh
@@ -13,12 +13,14 @@ ${use_superuser} apt-get update
 echo "Installing Dependency..."
 
 ${use_superuser} apt-get install -y nginx uwsgi uwsgi-plugin-python3 python3-pip python3-wheel git
-${use_superuser} pip3 install -r python_dependency.txt
+
 
 if [ ! -f "install.sh" ]; then
     git clone https://github.com/SilverBlogTeam/SilverBlog.git --depth=1 silverblog
     cd silverblog/install
 fi
+
+${use_superuser} pip3 install -r python_dependency.txt
 
 if [ ! -f "../start.json" ]; then
     cp -i ../example/pm2.example.json ../pm2.json
