acd5e4841670cd48b3fc0e18b6fdacacd2798f3a
qwe7002 <qwe7002@hotmail.com>
2018-02-28 08:54:24 +0800

    修改升级脚本

--- upgrade/upgrade_from_2.py
+++ upgrade/upgrade_from_2.py
@@ -1,8 +1,5 @@
 print("If the upgrade process fails, please re-upgrade")
 print("You can now remove the pypinyin support library")
 import os
-import time
-
-time.sleep(3)
 os.system("cd ./install && bash install_python_dependency.sh && sudo python3 -m pip uninstall pypinyin")
 print("We modified the startup script, please redeploy the startup script.")
