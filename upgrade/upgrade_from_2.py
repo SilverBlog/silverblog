838c176d38ae1932e9b02d094a440d4f8ea5dccf
qwe7002 <qwe7002@hotmail.com>
2018-03-15 19:47:42 +0800

    Update upgrade_from_2.py

--- upgrade/upgrade_from_2.py
+++ upgrade/upgrade_from_2.py
@@ -3,7 +3,7 @@ print("You can now remove the pypinyin support library")
 import os
 
 os.system("cd ./install && bash install_python_dependency.sh")
-uninstall_dependency = input('Do you want to enable this theme now? [y/N]')
+uninstall_dependency = input('Do you want to uninstall pypinyin now? [y/N]')
 if uninstall_dependency.lower() == 'yes' or uninstall_dependency.lower() == 'y':
     os.system("sudo python3 -m pip uninstall pypinyin")
 print("We modified the startup script, please redeploy the startup script.")
