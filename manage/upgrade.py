af5c58335541f8b23c1378a05f8b9579c157dcf1
qwe7002 <qwe7002@hotmail.com>
2018-03-23 18:05:08 +0800

    添加状态输出

--- manage/upgrade.py
+++ manage/upgrade.py
@@ -9,9 +9,11 @@ new_data_version = 3
 if not os.path.exists("./upgrade/current_version.json"):
     file.write_file("./upgrade/current_version.json", json.dumps({"current_data_version": new_data_version}))
 current_data_version = json.loads(file.read_file("./upgrade/current_version.json"))["current_data_version"]
+
 if not os.path.exists("./.git"):
     console.log("Error", "Not a git repository.")
     exit(1)
+
 repo = git.Repo("./")
 remote = repo.remote()
 def upgrade_check():
@@ -20,10 +22,15 @@ def upgrade_check():
         return True
     return False
 def upgrade_pull():
+    os.system("git status")
     if repo.is_dirty():
         console.log("Error",
-                    "The current warehouse is modified and can not be upgraded automatically. Please re-store the warehouse and try again.")
-        exit(1)
+                    "The current warehouse is modified and can not be upgraded automatically.")
+        checkout_repo = input('Do you want to restore these changes? [y/N]')
+        if checkout_repo.lower() == 'yes' or checkout_repo.lower() == 'y':
+            repo.index.checkout(force=True)
+        if repo.is_dirty():
+            exit(1)
     remote.pull()
 
     if current_data_version != new_data_version and os.path.exists(
