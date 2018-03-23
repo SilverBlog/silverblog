360cad822a73713ba29b2d2a716ae401a450739d
qwe7002 <qwe7002@hotmail.com>
2018-03-23 18:15:44 +0800

    添加权限

--- install/docker_install.sh
+++ install/docker_install.sh
@@ -13,6 +13,8 @@ fi
 cat << EOF >manage.sh
 #!/usr/bin/env bash
 docker exec -it silverblog python3 manage.py
+clear
 EOF
+chmod +x manage.sh
 echo "Before you start SilverBlog for the first time, run the following command to initialize the configuration:"
 echo "docker run -i -v \$(pwd):/home/silverblog --name="silverblog_manage" silverblog/silverblog python3 manage.py setting"
