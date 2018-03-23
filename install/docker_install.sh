367a30c747c0bafbc111aec1abf5bc785014eb9d
qwe7002 <qwe7002@hotmail.com>
2018-03-23 18:17:35 +0800

    添加转义符号

--- install/docker_install.sh
+++ install/docker_install.sh
@@ -17,4 +17,4 @@ clear
 EOF
 chmod +x manage.sh
 echo "Before you start SilverBlog for the first time, run the following command to initialize the configuration:"
-echo "docker run -i -v \$(pwd):/home/silverblog --name="silverblog_manage" silverblog/silverblog python3 manage.py setting"
+echo "docker run -i -v \$(pwd):/home/silverblog --name=\"silverblog_manage\" silverblog/silverblog python3 manage.py setting"
