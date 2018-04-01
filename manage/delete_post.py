724a22023198679a1a8ae871deef1ef559b977e5
qwe7002 <qwe7002@hotmail.com>
2018-04-01 22:38:52 +0800

    修正删除创建命令

--- manage/delete_post.py
+++ manage/delete_post.py
@@ -8,8 +8,8 @@ def delete(page_list, post_index):
     file_name = page_list[post_index]["name"]
     del page_list[post_index]
     file.write_file("./config/page.json", json.dumps(page_list, indent=4, sort_keys=False, ensure_ascii=False))
-    if not os.path.exists("./document/trash"):
-        os.mkdir("./document/trash")
+    if not os.path.exists("./trash"):
+        os.mkdir("./trash")
     if os.path.exists("./trash/{0}.md".format(file_name)):
         os.remove("./trash/{0}.md".format(file_name))
     shutil.move("./document/{0}.md".format(file_name), "./trash/{0}.md".format(file_name))
