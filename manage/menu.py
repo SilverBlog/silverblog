6ddd0b354fe3e96b4adbe6ba42e13f3ccdb20627
qwe7002 <qwe7002@hotmail.com>
2018-04-18 23:10:03 +0800

    添加安装提示

--- manage/menu.py
+++ manage/menu.py
@@ -111,8 +111,11 @@ def use_text_mode(args):
         try:
             import qrcode_terminal
         except ImportError:
-            import os
-            os.system("python3 -m pip install qrcode-terminal")
+            console.log("Error", "Please install the qrcode-terminal package to support this feature")
+            install_dependency = input('Do you want to install qrcode-terminal now? [y/N]')
+            if install_dependency.lower() == 'yes' or install_dependency.lower() == 'y':
+                import os
+                os.system("python3 -m pip install qrcode-terminal")
         if len(system_config["API_Password"]) == 0 or len(system_config["Project_URL"]) == 0:
             console.log("Error", "Check the API_Password and Project_URL configuration items")
             exit(1)
