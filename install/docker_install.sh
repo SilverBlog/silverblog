1f66ee12e34200b3c1c666b8caaab423a609d231
qwe7002 <qwe7002@hotmail.com>
2018-04-18 19:25:20 +0800

    修改脚本错误

--- install/docker_install.sh
+++ install/docker_install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-echo "Cloning silverblog..."
+
 if [ ! -f "initialization.sh" ]; then
+    echo "Cloning silverblog..."
     git clone https://github.com/SilverBlogTeam/SilverBlog.git --depth=1 silverblog
     cd silverblog/install
 fi
+
 echo "{\"install\":\"docker\"}" > install.lock
 ./initialization.sh
 cd ..
