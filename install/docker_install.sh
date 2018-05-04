b94d30e3a0b7a410229d78054ccd822f1e00b7b7
qwe7002 <qwe7002@hotmail.com>
2018-05-04 19:49:07 +0800

    添加脚本检测

--- install/docker_install.sh
+++ install/docker_install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if test $(ps h -o comm -p $$) = "sh"; then
+    echo "Please use bash to execute this script."
+    exit 1
+fi
 if [ ! -f "initialization.sh" ]; then
     echo "Cloning silverblog..."
     git clone https://github.com/SilverBlogTeam/SilverBlog.git --depth=1 silverblog
