008eed89df68fab900c0018986bdbb0642d448eb
qwe7002 <qwe7002@hotmail.com>
2018-05-14 23:17:27 +0800

    添加脚本

--- upgrade/upgrade_from_1.py
+++ upgrade/upgrade_from_1.py
@@ -31,3 +31,10 @@ for filename in os.listdir("./document/"):
         write_json = json.loads(read_file("./document/" + filename))
         write_json = change_time_fomart(write_json)
         write_file("./document/" + filename, json.dumps(write_json, indent=4, sort_keys=False, ensure_ascii=False))
+print("You can now remove the pypinyin support library")
+os.system("cd ./install && bash install_python_dependency.sh")
+uninstall_dependency = input('Do you want to uninstall pypinyin now? [y/N]')
+if uninstall_dependency.lower() == 'yes' or uninstall_dependency.lower() == 'y':
+    os.system("sudo python3 -m pip uninstall pypinyin")
+print("We modified the startup script, please redeploy the startup script.")
+print("The template format has been modified. Please upgrade the template.")
