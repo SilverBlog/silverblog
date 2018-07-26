2c3ef65fad8bf808fc585f4be375236ce5f537b3
qwe7002 <qwe7002@hotmail.com>
2018-07-26 15:44:12 +0800

    修改get方法

--- manage/get.py
+++ manage/get.py
@@ -15,7 +15,7 @@ def get_excerpt(filename):
     content = file.read_file(filename)
     excerpt_output = re.sub('<[\\s\\S]*?>[\\s\\S]*?<[\\s\\S]*?>|<[\\s\\S]*?/>', '', content)
     excerpt_output = re.sub('(!\[.*\]\([^)]*\))', '', excerpt_output)
-    excerpt_output = re.sub('\[(.*?)\].*(\(.*?/.*?\))', '\g<1>', excerpt_output)
+    excerpt_output = re.sub('\[(.*?)\](\(.*?/.*?\))', '\g<1>', excerpt_output)
     excerpt_output = re.sub('\*+|`|#+|>+|~+|=+|-+|_', '', excerpt_output)
     excerpt = excerpt_output.replace("\n", "").strip()
     if len(excerpt) > 140:
