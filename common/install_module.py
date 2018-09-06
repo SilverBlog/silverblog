db4eb4fb6f95c94d8277ff4a11918775b6feec54
qwe7002 <qwe7002@hotmail.com>
2018-09-06 23:06:08 +0800

    拆分install_module中install和import函数

--- common/install_module.py
+++ common/install_module.py
@@ -1,22 +1,28 @@
 import importlib
 import os
 
+from common import console
+
+
 def install_and_import(package):
     try:
         importlib.import_module(package)
     except ImportError:
-        from common import console
-        console.log("Error", "Please install the [{}] package to support this feature".format(package))
-        install_dependency = input('Do you want to install [{}] now? [y/N]'.format(package))
-        if install_dependency.lower() == 'yes' or install_dependency.lower() == 'y':
-            try:
-                from pip._internal import main
-            except Exception:
-                from pip import main
-            install_command = ['install']
-            if os.geteuid() != 0:
-                install_command.append("--user")
-            install_command.append(package)
-            main(install_command)
+        install_package(package)
     finally:
         globals()[package] = importlib.import_module(package)
+
+
+def install_package(package):
+    console.log("Error", "Please install the [{}] package to support this feature".format(package))
+    install_dependency = input('Do you want to install [{}] now? [y/N]'.format(package))
+    if install_dependency.lower() == 'yes' or install_dependency.lower() == 'y':
+        try:
+            from pip._internal import main
+        except Exception:
+            from pip import main
+        install_command = ['install']
+        if os.geteuid() != 0:
+            install_command.append("--user")
+        install_command.append(package)
+        main(install_command)
