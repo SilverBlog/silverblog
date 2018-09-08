98b4f66c37797646de346ecedf8f6503071e460e
qwe7002 <qwe7002@hotmail.com>
2018-09-08 13:57:39 +0800

    修改Access-Control-Allow

--- install/initialization.sh
+++ install/initialization.sh
@@ -19,13 +19,10 @@ server {
         uwsgi_pass unix:$(pwd)/config/unix_socks/control.sock;
         add_header 'Access-Control-Allow-Origin' "https://c.silverblog.org";
 	    add_header 'Access-Control-Allow-Credentials' "true";
+	    add_header 'Access-Control-Allow-Origin' "https://c.silverblog.org";
+        add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS, DELETE';
+        add_header 'Access-Control-Allow-Headers' 'reqid, nid, host, x-real-ip, x-forwarded-ip, event-type, event-id, accept, content-type';
         if (\$request_method = "OPTIONS") {
-            add_header 'Access-Control-Allow-Origin' "https://c.silverblog.org";
-            add_header 'Access-Control-Max-Age' 86400;
-            add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS, DELETE';
-            add_header 'Access-Control-Allow-Headers' 'reqid, nid, host, x-real-ip, x-forwarded-ip, event-type, event-id, accept, content-type';
-            add_header 'Content-Length' 0;
-            add_header 'Content-Type' 'text/plain, charset=utf-8';
             return 204;
         }
     }
