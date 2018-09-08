596a18f6d641de583644a1191d1c87ccfe107570
qwe7002 <qwe7002@hotmail.com>
2018-09-08 14:01:03 +0800

    修改nginx-config

--- install/docker_install.sh
+++ install/docker_install.sh
@@ -59,13 +59,9 @@ server {
         uwsgi_pass 127.0.0.1:5001;
         add_header 'Access-Control-Allow-Origin' "https://c.silverblog.org";
 	    add_header 'Access-Control-Allow-Credentials' "true";
+	    add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS, DELETE';
+        add_header 'Access-Control-Allow-Headers' 'reqid, nid, host, x-real-ip, x-forwarded-ip, event-type, event-id, accept, content-type';
         if (\$request_method = "OPTIONS") {
-            add_header 'Access-Control-Allow-Origin' "https://c.silverblog.org";
-            add_header 'Access-Control-Max-Age' 86400;
-            add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS, DELETE';
-            add_header 'Access-Control-Allow-Headers' 'reqid, nid, host, x-real-ip, x-forwarded-ip, event-type, event-id, accept, content-type';
-            add_header 'Content-Length' 0;
-            add_header 'Content-Type' 'text/plain, charset=utf-8';
             return 204;
         }
     }
