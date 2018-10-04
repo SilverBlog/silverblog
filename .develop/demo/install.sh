10f8bf68ee303596fea4bdae762db264d10d1592
qwe7002 <qwe7002@hotmail.com>
2018-10-04 14:19:58 +0800

    修改安装方法

--- .develop/demo/install.sh
+++ .develop/demo/install.sh
@@ -2,8 +2,8 @@
 set -o errexit
 
 bash /home/silverblog/install/initialization.sh
-sed -i '''s/.\/config\/unix_socks\/main.sock/0.0.0.0:5000/g' uwsgi.json
-sed -i '''s/.\/config\/unix_socks\/control.sock/0.0.0.0:5001/g' uwsgi.json
+sed -i '''s@./config/unix_socks/main.sock@0.0.0.0:5000@g' uwsgi.json
+sed -i '''s@./config/unix_socks/control.sock@0.0.0.0:5001@g' uwsgi.json
 cp -f /home/silverblog/.develop/demo/page.json /home/silverblog/config/page.json
 cp -f /home/silverblog/.develop/demo/menu.json /home/silverblog/config/menu.json
 cp -f /home/silverblog/.develop/demo/system.json /home/silverblog/config/system.json
