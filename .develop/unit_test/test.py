df277d9343bbe0968b149f5e35b46bb449b9cb05
qwe7002 <qwe7002@hotmail.com>
2018-12-28 21:08:01 +0800

    修改测试

--- .develop/unit_test/test.py
+++ .develop/unit_test/test.py
@@ -1,3 +1,5 @@
+import hashlib
+import hmac
 import subprocess
 import time
 
@@ -20,43 +22,70 @@ def post(url):
     print(r.status_code)
     return False
 
+
+def build_hash(password, content, send_time):
+    return hmac.new(str(password + str(send_time)).encode('utf-8'), str(content).encode('utf-8'),
+                    digestmod=hashlib.sha512).hexdigest()
+
 get_list = ["", "post/demo-article", "rss"]
 for item in get_list:
     if not get(item):
         print("ERROR:" + item)
         exit(1)
-# password =
-post_list = ["control/system_info", "control/v2/get/list/post", "control/get_list/menu"]
+
+password_hash = "a238f3ead3fcf2df15e43e0558b5cc374ede284e17a6f03c396a5014606e6901"
+
+post_list = ["control/system_info", "control/v2/get/list/post", "control/v2/get/list/menu"]
 for item in post_list:
     if not post(item):
         print("ERROR:" + item)
         exit(1)
 
-r = requests.post(host + "/control/v2/get/content/post", json={"post_id": 0})
+r = requests.post(host + "/control/v2/get/content/post", json={"post_uuid": "c3472075-440f-42d9-9421-3e83d46568d4"})
 print("get_content:" + r.text)
 if not r.json()["status"]:
     print("ERROR:get_content")
     exit(1)
 
-r = requests.post(host + "/control/v2/new", json={"content": "test", "name": "test", "title": "title",
-                                                        "sign": "bd35db586ff3f4617d90511546aa6e3b"})
+name = "test"
+title = "test_title"
+content = "this is a test"
+
+sign_text = title + name + hashlib.sha512(content.encode("utf-8")).hexdigest()
+send_time = int(round(time.time() * 1000))
+sign = build_hash(password_hash, sign_text, send_time)
+r = requests.post(host + "/control/v2/new", json={"content": content, "name": name, "title": title,
+                                                  "sign": sign, "send_time": send_time})
 print("new:" + r.text)
 if not r.json()["status"]:
     print("ERROR:new")
     exit(1)
 
+new_uuid = r.json()["uuid"]
+name = "test2"
+title = "test2_title"
+content = "this is a test2"
+sign_text = new_uuid + title + name + hashlib.sha512(content.encode("utf-8")).hexdigest()
+send_time = int(round(time.time() * 1000))
+sign = build_hash(password_hash, sign_text, send_time)
+
 r = requests.post(host + "/control/v2/edit/post",
-                  json={"post_id": 0, "content": "test2", "name": "test", "title": "title",
-                        "sign": "bd35db586ff3f4617d90511546aa6e3b"})
+                  json={"post_uuid": new_uuid, "content": content, "name": name, "title": title,
+                        "sign": sign, "send_time": send_time})
 print("edit:" + r.text)
 if not r.json()["status"]:
     print("ERROR:edit")
     exit(1)
-r = requests.post(host + "/control/v2/delete", json={"post_id": 0, "sign": "f48659e554318cf884d0edbc7038c52c"})
+
+sign_text = new_uuid + title + name
+send_time = int(round(time.time() * 1000))
+sign = build_hash(password_hash, sign_text, send_time)
+r = requests.post(host + "/control/v2/delete", json={"post_uuid": new_uuid, "sign": sign, "send_time": send_time})
 print("delete:" + r.text)
 if not r.json()["status"]:
     print("ERROR:delete")
     exit(1)
+
 nginx_progress.kill()
 main_progress.kill()
 control_progress.kill()
