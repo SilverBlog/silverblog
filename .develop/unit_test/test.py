047364c2f729f7f4886481b9fb1f2463427541c5
qwe7002 <qwe7002@hotmail.com>
2018-12-28 21:24:15 +0800

    修改提交方法

--- .develop/unit_test/test.py
+++ .develop/unit_test/test.py
@@ -14,6 +14,7 @@ def get(url):
     r = requests.get(host + url)
     if r.status_code == 200:
         return True
+    print(r.status_code)
     return False
 def post(url):
     r = requests.post(host + url)
@@ -33,15 +34,15 @@ for item in get_list:
         print("ERROR:" + item)
         exit(1)
 
-password_hash = "a238f3ead3fcf2df15e43e0558b5cc374ede284e17a6f03c396a5014606e6901"
+
 
 post_list = ["control/system_info", "control/v2/get/list/post", "control/v2/get/list/menu"]
 for item in post_list:
-    if not post(item):
+    if not get(item):
         print("ERROR:" + item)
         exit(1)
-
-r = requests.post(host + "/control/v2/get/content/post", json={"post_uuid": "c3472075-440f-42d9-9421-3e83d46568d4"})
+password_hash = "a238f3ead3fcf2df15e43e0558b5cc374ede284e17a6f03c396a5014606e6901"
+r = requests.post(host + "control/v2/get/content/post", json={"post_uuid": "c3472075-440f-42d9-9421-3e83d46568d4"})
 print("get_content:" + r.text)
 if not r.json()["status"]:
     print("ERROR:get_content")
@@ -54,7 +55,7 @@ content = "this is a test"
 sign_text = title + name + hashlib.sha512(content.encode("utf-8")).hexdigest()
 send_time = int(round(time.time() * 1000))
 sign = build_hash(password_hash, sign_text, send_time)
-r = requests.post(host + "/control/v2/new", json={"content": content, "name": name, "title": title,
+r = requests.post(host + "control/v2/new", json={"content": content, "name": name, "title": title,
                                                   "sign": sign, "send_time": send_time})
 print("new:" + r.text)
 if not r.json()["status"]:
@@ -69,7 +70,7 @@ sign_text = new_uuid + title + name + hashlib.sha512(content.encode("utf-8")).he
 send_time = int(round(time.time() * 1000))
 sign = build_hash(password_hash, sign_text, send_time)
 
-r = requests.post(host + "/control/v2/edit/post",
+r = requests.post(host + "control/v2/edit/post",
                   json={"post_uuid": new_uuid, "content": content, "name": name, "title": title,
                         "sign": sign, "send_time": send_time})
 print("edit:" + r.text)
@@ -80,7 +81,7 @@ if not r.json()["status"]:
 sign_text = new_uuid + title + name
 send_time = int(round(time.time() * 1000))
 sign = build_hash(password_hash, sign_text, send_time)
-r = requests.post(host + "/control/v2/delete", json={"post_uuid": new_uuid, "sign": sign, "send_time": send_time})
+r = requests.post(host + "control/v2/delete", json={"post_uuid": new_uuid, "sign": sign, "send_time": send_time})
 print("delete:" + r.text)
 if not r.json()["status"]:
     print("ERROR:delete")
