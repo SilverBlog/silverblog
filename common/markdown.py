7d4dcdb81e601e26083295874153510e67621e16
qwe7002 <qwe7002@hotmail.com>
2019-06-21 19:37:49 +0800

    修改commit方法

--- common/markdown.py
+++ common/markdown.py
@@ -1,14 +1,20 @@
 import hoedown
 
+
 class image_renderer(hoedown.HtmlRenderer):
     def image(self, src, title, alt):
-        return "<img src=\"{}\" alt=\"{}\" title=\"{}\" lazyload=\"on\">".format(src,alt,title)
+        param = ""
+        if title is not None:
+            param = " title=\"{}\"".format(title)
+        if alt is not None:
+            param = param + "alt=\"{}\"".format(alt)
+        return "<img src=\"{}\" lazyload=\"on\"{}>".format(src, param)
 
 
-def markdown(system_config,content):
+def markdown(system_config, content):
     renderer = hoedown.HtmlRenderer()
     if 'Lazyload' in system_config and system_config['Lazyload']:
         renderer = image_renderer()
-    md = hoedown.Markdown(renderer,extensions=hoedown.EXT_FENCED_CODE | hoedown.EXT_HIGHLIGHT |
-                                            hoedown.EXT_AUTOLINK | hoedown.EXT_TABLES  | hoedown.EXT_STRIKETHROUGH | hoedown.EXT_UNDERLINE).render
+    md = hoedown.Markdown(renderer, extensions=hoedown.EXT_FENCED_CODE | hoedown.EXT_HIGHLIGHT |
+                                               hoedown.EXT_AUTOLINK | hoedown.EXT_TABLES | hoedown.EXT_STRIKETHROUGH | hoedown.EXT_UNDERLINE).render
     return md(content)
