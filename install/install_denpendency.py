0716487daf8c4d10e63ba3c89cf2a21dc631efa6
qwe7002 <qwe7002@hotmail.com>
2019-06-29 21:57:13 +0800

    修改依赖安装，关闭默认拼音slug的功能

--- install/install_denpendency.py
+++ install/install_denpendency.py
@@ -2,32 +2,36 @@
 import os
 import sys
 import importlib
-
+install_command = ['install', "-U"]
 def install():
     try:
         from pip import main as pip_main
     except ImportError:
         from pip._internal import main as pip_main #Loading a pip with version < 10.0.0
-    install_command = ['install', "-U"]
     if os.geteuid() != 0:
         print("The current user is not root and is installed in user mode.")
         install_command.append("--user")
     if sys.version_info < (3, 4):
         print("Current python version is lower than 3.4, need to install asyncio.")
         install_command.append("asyncio")
+
+    test_install("qrcode_terminal","Do you want to install [qrcode_terminal] to support QR code login?")
+    test_install("xpinyin","Do you want to install [xpinyin] to support pinyin slug?")
+    install_command.extend(["Flask", "hoedown", "pyrss2gen", "gitpython", "requests", "watchdog"])
+    pip_main(install_command)
+
+def test_install(name,question):
     try:
-        importlib.import_module("qrcode_terminal")
+        importlib.import_module(name)
     except ImportError:
         install_dependency = "n"
         try:
-            install_dependency = str(input("Do you want to install [qrcode_terminal] to support QR code login? (y/N)"))
+            install_dependency = str(input( "(y/N)").format(question))
         except EOFError:
             print("N")
         if install_dependency.lower() == 'y':
-            install_command.append("qrcode_terminal")
-    install_command.extend(["Flask", "hoedown", "xpinyin", "pyrss2gen", "gitpython", "requests", "watchdog"])
-    pip_main(install_command)
-
+            global install_command
+            install_command.append(name)
 
 def main():
     install()
